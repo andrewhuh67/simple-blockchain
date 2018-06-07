450c6a9f742746534d3df0e9ab632049fd6a2022
Andrew <andrewhuh67@gmail.com>
2018-06-07 12:39:54 -0700

    added functions for blockchain

--- blockchain.py
+++ blockchain.py
@@ -3,20 +3,40 @@ class Blockchain(object):
         self.chain = []
         self.current_transactions = []
 
-	def new_block(self):
-        # Creates a new Block and adds it to the chain
-        pass
-    
-    def new_transaction(self):
-        # Adds a new transaction to the list of transactions
-        pass
+        self.new_block(previous_hash=1, proof=100)
+
+	def new_block(self, proof, previous_hash=None):
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+        
+    def new_transaction(self, sender, recipient, amount):
+    	
+    	self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
-        # Hashes a Block
-        pass
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
 
     @property
     def last_block(self):
-        # Returns the last Block in the chain
-        pass
+        return self.chain[-1]
+
