57d6b06297fc8ba6bc263bd4a756045d8257f0d7
Andrew <andrewhuh67@gmail.com>
2018-06-07 12:49:56 -0700

    started adding flask component

--- blockchain.py
+++ blockchain.py
@@ -1,42 +1,97 @@
-class Blockchain(object):
-    def __init__(self):
-        self.chain = []
-        self.current_transactions = []
+import hashlib
+import json
+from textwrap import dedent
+from time import time
+from uuid import uuid4
 
-        self.new_block(previous_hash=1, proof=100)
+from flask import Flask, jsonify, request
+
+
+class Blockchain(object):
+	def __init__(self):
+		self.chain = []
+		self.current_transactions = []
+
+		self.new_block(previous_hash=1, proof=100)
 
 	def new_block(self, proof, previous_hash=None):
 
-        block = {
-            'index': len(self.chain) + 1,
-            'timestamp': time(),
-            'transactions': self.current_transactions,
-            'proof': proof,
-            'previous_hash': previous_hash or self.hash(self.chain[-1]),
-        }
+		block = {
+			'index': len(self.chain) + 1,
+			'timestamp': time(),
+			'transactions': self.current_transactions,
+			'proof': proof,
+			'previous_hash': previous_hash or self.hash(self.chain[-1]),
+		}
 
-        self.current_transactions = []
+		self.current_transactions = []
 
-        self.chain.append(block)
-        return block
-        
-    def new_transaction(self, sender, recipient, amount):
-    	
-    	self.current_transactions.append({
-            'sender': sender,
-            'recipient': recipient,
-            'amount': amount,
-        })
-        return self.last_block['index'] + 1
+		self.chain.append(block)
+		return block
+		
+	def new_transaction(self, sender, recipient, amount):
+		
+		self.current_transactions.append({
+			'sender': sender,
+			'recipient': recipient,
+			'amount': amount,
+		})
+		return self.last_block['index'] + 1
 
-    @staticmethod
-    def hash(block):
-        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
-        block_string = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(block_string).hexdigest()
+	def proof_of_work(self, last_proof):
+
+		proof = 0
+		while self.valid_proof(last_proof, proof) is False:
+			proof += 1
+
+		return proof
+
+	@staticmethod
+	def valid_proof(last_proof, proof):
+
+		guess = f'{last_proof}{proof}'.encode()
+		guess_hash = hashlib.sha256(guess).hexdigest()
+		return guess_hash[:4] == "0000"
+
+	@staticmethod
+	def hash(block):
+		# We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+		block_string = json.dumps(block, sort_keys=True).encode()
+		return hashlib.sha256(block_string).hexdigest()
 
 
-    @property
-    def last_block(self):
-        return self.chain[-1]
+	@property
+	def last_block(self):
+		return self.chain[-1]
+
+# Instantiate our Node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "We'll add a new transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
 
